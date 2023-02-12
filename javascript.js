be07fc66f34083a5e5fc081c5db317d31ac71dc5
Stephanie Henson <108551327+s-hens@users.noreply.github.com>
2023-02-12 22:05:12 +0000

    Create gameboard array w/ module

--- javascript.js
+++ javascript.js
@@ -1,3 +1,21 @@
+// Create game board using module
+const gameboard = [];
+
+const boardModule = (() => {
+    for (let i = 0; i < 9; i++) {
+        //The div
+        const div = document.createElement("div");
+        div.setAttribute("data-index", `${i}`);
+        document.querySelector("main").appendChild(div);
+        //The object
+        const box = {fill: "test"};
+        gameboard.push(box);
+    }
+})();
+
+console.table(gameboard);
+console.log(gameboard.at(2).fill);
+
 // Create array of players using factory function
 const players = [];
 
