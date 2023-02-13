76d5113bd147425c9af937b45da2588cd94b10d4
Stephanie Henson <108551327+s-hens@users.noreply.github.com>
2023-02-13 16:04:51 +0000

    For any cell, click to change marker/identify neighbors

--- javascript.js
+++ javascript.js
@@ -2,19 +2,41 @@
 const gameboard = [];
 
 const boardModule = (() => {
+    let neighbors;
+
     for (let i = 0; i < 9; i++) {
-        //The div
-        const div = document.createElement("div");
-        div.setAttribute("data-index", `${i}`);
-        document.querySelector("main").appendChild(div);
-        //The object
-        const box = {fill: "test"};
+        //Object
+        const box = {
+            fill: "empty",
+            neighbors: [i - 1,  i + 1, i - 3, i + 3]
+        };
         gameboard.push(box);
+        //Div
+        const div = document.createElement("div");
+        div.classList.add("box");
+        div.setAttribute("data-index", `${i}`); //data-index of div matches gameboard.indexOf(box)
+        div.innerText = `${box.fill}`; //div innerText matches box.fill
+        document.querySelector("main").appendChild(div);
+        //Div event listeners
+        div.addEventListener("click", addMark);
+    }
+
+    function addMark() {
+        this.innerText = `${human.marker}`;
+        gameboard.at(this.getAttribute("data-index")).fill = human.marker; //div innerText STILL matches box.fill
+        console.table(gameboard);
+
+        neighbors = gameboard.at(this.getAttribute("data-index")).neighbors;
+        console.table(neighbors);
+        neighbors.forEach(neighbor =>
+            document.querySelector(`[data-index="${neighbor}"]`).setAttribute('style', 'background-color: yellow')
+        );
     }
 })();
 
 console.table(gameboard);
-console.log(gameboard.at(2).fill);
+
+
 
 // Create array of players using factory function
 const players = [];
@@ -26,5 +48,3 @@ const playerFactory = (name, marker, score) => {
 
 const human = playerFactory("human", "x", 0);
 const computer = playerFactory("computer", "o", 0);
-
-console.table(players);
