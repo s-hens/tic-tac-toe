ecdaf0d53df9d9ffd9b291b02a92c010ea5abadd
Stephanie Henson <108551327+s-hens@users.noreply.github.com>
2023-02-13 17:36:42 +0000

    New strategy: check board for 3-in-a-row on each turn

--- javascript.js
+++ javascript.js
@@ -1,44 +1,6 @@
-// Create game board using module
-const gameboard = [];
-
-const boardModule = (() => {
-    let neighbors;
-
-    for (let i = 0; i < 9; i++) {
-        //Object
-        const box = {
-            fill: "empty",
-            neighbors: [i - 1,  i + 1, i - 3, i + 3]
-        };
-        gameboard.push(box);
-        //Div
-        const div = document.createElement("div");
-        div.classList.add("box");
-        div.setAttribute("data-index", `${i}`); //data-index of div matches gameboard.indexOf(box)
-        div.innerText = `${box.fill}`; //div innerText matches box.fill
-        document.querySelector("main").appendChild(div);
-        //Div event listeners
-        div.addEventListener("click", addMark);
-    }
-
-    function addMark() {
-        this.innerText = `${human.marker}`;
-        gameboard.at(this.getAttribute("data-index")).fill = human.marker; //div innerText STILL matches box.fill
-        console.table(gameboard);
-
-        neighbors = gameboard.at(this.getAttribute("data-index")).neighbors;
-        console.table(neighbors);
-        neighbors.forEach(neighbor =>
-            document.querySelector(`[data-index="${neighbor}"]`).setAttribute('style', 'background-color: yellow')
-        );
-    }
-})();
-
-console.table(gameboard);
-
-
-
-// Create array of players using factory function
+//------------------------------------------------//
+// Create array of players using factory function //
+//------------------------------------------------//
 const players = [];
 
 const playerFactory = (name, marker, score) => {
@@ -48,3 +10,70 @@ const playerFactory = (name, marker, score) => {
 
 const human = playerFactory("human", "x", 0);
 const computer = playerFactory("computer", "o", 0);
+
+//-------------------------------------------------------//
+// Create array of win conditions using factory function //
+//-------------------------------------------------------//
+const triples = [];
+
+const tripleFactory = (first, second, third) => {
+    triples.push({first, second, third});
+    return {first, second, third};
+}
+
+// Horizontal triples
+tripleFactory(0, 1, 2);
+tripleFactory(3, 4, 5);
+tripleFactory(6, 7, 8);
+// Vertical triples
+tripleFactory(0, 3, 6);
+tripleFactory(1, 4, 7);
+tripleFactory(2, 5, 8);
+// Diagonal triples
+tripleFactory(0, 4, 8);
+tripleFactory(2, 4, 6);
+
+//--------------------------------//
+// Create game board using module //
+//--------------------------------//
+const gameboard = [];
+
+const boardModule = (() => {
+
+    for (let i = 0; i < 9; i++) {
+        // Object
+        const box = {fill: "empty"};
+        gameboard.push(box);
+        // Div
+        const div = document.createElement("div");
+        div.classList.add("box");
+        div.setAttribute("data-index", `${i}`); //data-index of div matches gameboard.indexOf(box)
+        div.innerText = `${box.fill}`; //div innerText matches box.fill
+        document.querySelector("main").appendChild(div);
+        // Div event listeners
+        div.addEventListener("click", addMark);
+    }
+
+    function addMark() {
+        this.innerText = `${human.marker}`;
+        gameboard.at(this.getAttribute("data-index")).fill = human.marker; //div innerText STILL matches box.fill
+        console.table(gameboard);
+        checkBoard();
+    }
+
+    function checkBoard() {
+        triples.forEach(triple => {
+            //console.log(gameboard.at(triple.first).fill);
+            if (gameboard.at(triple.first).fill === gameboard.at(triple.second).fill
+             && gameboard.at(triple.second).fill === gameboard.at(triple.third).fill
+             && gameboard.at(triple.first). fill != "empty"
+             ) {
+                console.log("win!");
+            } else {
+                console.log("no win yet")
+            };
+        });
+    }
+})();
+
+//console.table(gameboard);
