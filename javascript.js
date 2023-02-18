e41b2b616ad7e275bb2448ec35b77248fa225764
s-hens <108551327+s-hens@users.noreply.github.com>
2023-02-18 22:01:56 +0000

    Reorganise code, taking new approach

--- javascript.js
+++ javascript.js
@@ -1,104 +1,111 @@
-//------------------------------------------------//
-// Create array of players using factory function //
-//------------------------------------------------//
+// Create array of players using factory function
 const players = [];
 
 const playerFactory = (name, marker, score) => {
     players.push({name, marker, score});
     return {name, marker, score};
-}
+};
 
 const human = playerFactory("human", "x", 0);
 const computer = playerFactory("computer", "o", 0);
 
-//-------------------------------------------------------//
-// Create array of win conditions using factory function //
-//-------------------------------------------------------//
+// Create array of win conditions using factory function
 const triples = [];
 
-const tripleFactory = (first, second, third) => {
-    triples.push({first, second, third});
-    return {first, second, third};
+const tripleFactory = (a, b, c) => {
+    triples.push({a, b, c});
+    return {a, b, c};
 }
 
-// Horizontal triples
 tripleFactory(0, 1, 2);
 tripleFactory(3, 4, 5);
 tripleFactory(6, 7, 8);
-// Vertical triples
 tripleFactory(0, 3, 6);
 tripleFactory(1, 4, 7);
 tripleFactory(2, 5, 8);
-// Diagonal triples
 tripleFactory(0, 4, 8);
 tripleFactory(2, 4, 6);
 
-//--------------------------------//
-// Create game board using module //
-//--------------------------------//
+// Create game board using module & IIFE
 const gameboard = [];
+let availableBoxes = [];
 
 const boardModule = (() => {
 
+    // Create board array using factory function
+    const boxFactory = (index, marker, available) => {
+        gameboard.push({index, marker, available});
+        return {index, marker, available};
+    };
+
     for (let i = 0; i < 9; i++) {
-        // Object
-        const box = {fill: "empty"};
-        gameboard.push(box);
-        // Div
-        const div = document.createElement("div");
-        div.classList.add("box");
-        div.setAttribute("data-index", `${i}`); //data-index of div matches gameboard.indexOf(box)
-        div.innerText = `${box.fill}`; //div innerText matches box.fill
-        document.querySelector("main").appendChild(div);
-        // Div event listeners
-        div.addEventListener("click", handleTurn);
-    }
+        boxFactory(i, "empty", true);
+    };
 
-    let active;
-    let gameOver = false;
+    // Update board
+    function updateBoard() {
+        // Update DOM based on gameboard array
+        document.querySelector("main").innerHTML = ``;
+        gameboard.forEach(box => {
+            const div = document.createElement("div");
+            div.classList.add("box");
+            div.setAttribute("data-index", `${box.index}`);
+            div.innerText = `${box.marker}`;
+            document.querySelector("main").appendChild(div);
+        });
+        // Update available boxes array and event listeners to available boxes
+        availableBoxes = [];
+        gameboard.forEach(box => {
+            if (box.available === true) {
+                availableBoxes.push(box);
+                document.querySelector(`[data-index="${box.index}"]`).classList.add("available");
+                document.querySelector(`[data-index="${box.index}"]`).addEventListener("click", handleTurn);
+            }
+        });
+        checkForWin();
+    };
 
-    function handleTurn() {
-        // Place marker
-        active = human;
-        this.innerText = `${active.marker}`;
-        gameboard.at(this.getAttribute("data-index")).fill = active.marker; //div innerText STILL matches box.fill
-        // Check for win
-        checkForWin();
-        if (gameOver === true) {
-            return;
-        };
-        //Computer turn: hard
-        active = computer;
-        hardAI();
-        console.table(gameboard);
-        // Check for win
-        checkForWin();
-        if (gameOver === true) {
-            return;
-        };
-    }
-    
+    // Check win conditions
     function checkForWin() {
         triples.forEach(triple => {
             if (
-            gameboard.at(triple.first).fill === gameboard.at(triple.second).fill &&
-            gameboard.at(triple.second).fill === gameboard.at(triple.third).fill &&
-            gameboard.at(triple.first).fill != "empty") {
-                active.score = active.score + 1;
-                console.log(`${active.name} win. ${active.score}`);
-                gameOver = true;
+            gameboard.at(triple.a).marker === gameboard.at(triple.b).marker &&
+            gameboard.at(triple.b).marker === gameboard.at(triple.c).marker &&
+            gameboard.at(triple.a).available === false) {
+                console.log(`Congrats ${active.name}, you win!`);
             };
         });
-    }
+    };
 
+    // Computer AI: Easy
     function easyAI() {
-        let availableMoves = gameboard.filter(box => box.fill === "empty");
-        let computerMove = availableMoves[Math.floor(Math.random() * availableMoves.length)];
-        document.querySelector(`[data-index="${gameboard.indexOf(computerMove)}"]`).innerText = `${computer.marker}`;
-        gameboard.at(gameboard.indexOf(computerMove)).fill = computer.marker;
+        let computerMove = availableBoxes[Math.floor(Math.random() * availableBoxes.length)];
+        console.log(computerMove);
+        gameboard.at(computerMove.index).marker = active.marker;
+        updateBoard();
     }
 
-    function hardAI() {
+    // Handle turn
+    let active;
+
+    function handleTurn() {
+        // Human's turn
+        active = human;
+        console.log(active);
+        gameboard.at(this.getAttribute("data-index")).marker = active.marker;
+        gameboard.at(this.getAttribute("data-index")).available = false;
+        updateBoard();
+        // Computer's turn
+        active = computer;
+        easyAI();
+    };
+
+    updateBoard();
+    
+})();
+
+/*
+function hardAI() {
         let goodMoves = [];
         let availableMoves = [];
 
@@ -146,5 +153,4 @@ const boardModule = (() => {
             console.log("using smart move choice");
         };
     }
-    
-})();
+*/
